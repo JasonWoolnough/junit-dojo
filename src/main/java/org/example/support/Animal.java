422fab0a7418c324458d9557c11d8d06e962edd3
Faheem Asvat <faheem.asvat@entelect.co.za>
2020-03-03 10:33:19 +0200

    both tests for exercise 1 and 2

--- src/main/java/org/example/support/Animal.java
+++ src/main/java/org/example/support/Animal.java
@@ -8,7 +8,10 @@ public class Animal {
     private String family;
     private String species;
 
-    private Animal(){};
+    private Animal() {
+    }
+
+    ;
 
     public Animal(String name, Long animalCode) {
         AnimalE animalE = AnimalE.fromCode(animalCode);
@@ -43,4 +46,29 @@ public class Animal {
             throw new AnimalNotFoundException(code);
         }
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
 }
--- src/test/java/org/example/Exercise2Test.java
+++ src/test/java/org/example/Exercise2Test.java
@@ -1,15 +1,22 @@
 package org.example;
 
-import org.example.support.AnimalRepo;
+import org.example.support.*;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 /**
  * This one is deceptive. It initially looks like it will be a single test.
- *
+ * <p>
  * But don't be fooled! This class invokes the Animal class constructor, which has a bunch of logic in it.
  * Because Animal is not an injected dependency but a stateful object instantiated INSIDE the tested code, we need to test it too.
  */
@@ -27,6 +34,18 @@ public class Exercise2Test {
      */
     @Test
     public void saveAnimal_shouldSaveAnimal() {
+        Animal animal = new Animal("Faheem", 1L);
+
+        exercise2.saveAnimal("Faheem", 1L);
+
+        ArgumentCaptor<Animal> captor = ArgumentCaptor.forClass(Animal.class);
+
+        verify(animalRepo).save(captor.capture());
+
+        Animal animal1 = captor.getValue();
+
+        assertThat(animal1.getName(), equalTo("Faheem"));
+        assertThat(animal1.getFamily(), equalTo("Cat"));
 
     }
 
@@ -35,16 +54,30 @@ public class Exercise2Test {
      */
     @Test
     public void saveAnimal_shouldThrowAnimalNotFoundExceptionIfNullAnimalCodeProvided() {
-
+        try {
+            exercise2.saveAnimal("Faheem", null);
+            Assert.fail("Should have failed");
+        } catch (AnimalNotFoundException ae) {
+            assertThat(ae.getMessage(), equalTo("Could not find animal with code: " + null));
+        } catch (Throwable t) {
+            Assert.fail("Should have thrown the AnimalNotFoundException but instead it through some weird shit:" + t.getMessage());
+        }
     }
 
     /**
      * Call saveAnimal with a non-valid Long. A negative long like -1L or an out-of-range long like 10L will do.
-     *
+     * <p>
      * This isn't technically necessary for code coverage, but null safety and out-of-range tests are normally useful as separate tests.
      */
     @Test
     public void saveAnimal_shouldThrowAnimalNotFoundExceptionIfInvalidAnimalCodeProvided() {
-
+        try {
+            exercise2.saveAnimal("Faheem", -1L);
+            Assert.fail("Should have failed");
+        } catch (AnimalNotFoundException ae) {
+            assertThat(ae.getMessage(), equalTo("Could not find animal with code: " + -1L));
+        } catch (Throwable t) {
+            Assert.fail("Should have thrown the AnimalNotFoundException but instead it through some weird shit:" + t.getMessage());
+        }
     }
 }
