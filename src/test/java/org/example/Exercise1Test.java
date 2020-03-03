762f46016dedfc73be372abd9b49fb77f94b8562
Faheem Asvat <faheem.asvat@entelect.co.za>
2020-03-03 10:05:39 +0200

    Some getter and setters cause I can't pick up lombok.

--- src/test/java/org/example/Exercise1Test.java
+++ src/test/java/org/example/Exercise1Test.java
@@ -1,11 +1,17 @@
 package org.example;
 
+import org.example.support.Car;
 import org.example.support.CarAdapter;
+import org.example.support.CarDto;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.when;
 
 /**
  * This class can be fully tested using only one method.
@@ -26,9 +32,20 @@ public class Exercise1Test {
 
     @InjectMocks
     Exercise1 exercise1;
-
     @Test
     public void saveCar_shouldSaveAndReturnCar() {
+        CarDto carDto = new CarDto();
+        carDto.setColour("blue");
+        carDto.setMake("Honda");
+        carDto.setModel("Civic");
+        Car savecar = new Car(carDto);
 
+        ArgumentCaptor<CarDto> captor = ArgumentCaptor.forClass(CarDto.class);
+
+        when(carAdapter.save(captor.capture())).thenReturn(carDto);
+
+        Car savedcar = this.exercise1.saveCar(savecar);
+        Assert.assertNotNull(savedcar);
+        Assert.assertEquals(savecar,savedcar);
     }
 }
